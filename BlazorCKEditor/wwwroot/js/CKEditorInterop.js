be8d30dcfa221c38c21279e3de05f22caa14bf6d
opic <floresopic@gmail.com>
2020-04-03 17:08:09 +0800

    feature(autosave): install autosave plugin

--- BlazorCKEditor/wwwroot/js/CKEditorInterop.js
+++ BlazorCKEditor/wwwroot/js/CKEditorInterop.js
@@ -3,10 +3,24 @@
 
     return {
         init(id, dotNetReference) {
+            // config is optional
             ClassicEditor
-                .create(document.getElementById(id))
+                // The autosave feature listens to the editor.model.document#change:data event, throttles it and executes the config.autosave.save() function.
+                .create(document.getElementById(id), {
+                    autosave: {
+                        save(editor) {
+                            let data = editor.getData();
+                            console.log(data);
+                            return dotNetReference.invokeMethodAsync('EditorAutoSave', data);
+                            // The saveData() function must return a promise
+                            // which should be resolved when the data is successfully saved.
+                            // return saveData(editor[id].getData());
+                        }
+                    }
+                })
                 .then(editor => {
                     editors[id] = editor;
+                    // listening to change data
                     editor.model.document.on('change:data', () => {
                         let data = editor.getData();
 
@@ -16,6 +30,8 @@
                             data = null;
 
                         dotNetReference.invokeMethodAsync('EditorDataChanged', data);
+
+                        displayStatus(editor);
                     });
                 })
                 .catch(error => console.error(error));
@@ -27,3 +43,20 @@
         }
     };
 })();
+
+
+// Update the "Status: Saving..." info.
+function displayStatus(editor) {
+    const pendingActions = editor.plugins.get('PendingActions');
+    const statusIndicator = document.querySelector('#editor-status');
+
+    pendingActions.on('change:hasAny', (evt, propertyName, newValue) => {
+        if (newValue) {
+            statusIndicator.classList.add('alert-danger');
+            statusIndicator.classList.remove('saved');
+        } else {
+            statusIndicator.classList.remove('alert-danger');
+            statusIndicator.classList.add('saved');
+        }
+    });
+}
